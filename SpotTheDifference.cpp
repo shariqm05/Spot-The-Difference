0d46cf9a8f0642524be10d79b95eb45f3eec900c
shariqm05 <shariqm2005@gmail.com>
2024-09-18 01:52:18 -0400

    Add files via upload
    Added the implementation for the loadFile and clearArray functions. Full debugged but not tested yet.

--- SpotTheDifference.cpp
+++ SpotTheDifference.cpp
@@ -5,11 +5,13 @@ Date: 9-26-24
 Section: 22
 E-mail: shariqm1@umbc.edu
 
-Desc: A program that runs a "Spot the Difference" game using ASCII art. The user load a file using commands from the menu, and then can play the game.
+Desc: A program that runs a "Spot the Difference" game using ASCII art.
+The user loads a file using commands from the menu, and then can play the game.
 Two images are displayed. the first is the original and the second having one character altered.
 The player must guess the correct row and column of the changed character to win.
 */
 
+//libraries imported
 #include <iostream>
 #include <fstream>
 #include <cmath>
@@ -21,15 +23,31 @@ using namespace std;
 const int ROWS = 10;
 const int COLS = 21;
 
-bool loadFile(char charArr[ROWS][COLS]) {
+void loadFile(char charArr[ROWS][COLS]) {
 /*
 -Asks the user to input the name of the file they want to load
 -If the file is not found, a message is printed saying so
 -If the file is found, a "Done" message will print
 -Stores every character in the file in the cahracter array
+*/ 
+    string fileName;
+    cout << "Enter the name of the file(.txt) to load: " << endl;
+    cin >> fileName;
+    ifstream fileArt(fileName);
 
-return: T/F for whether the file was loaded
-*/    
+    //opening and processing file into the character array
+    if (fileArt.is_open()){
+        for(int r = 0; r < ROWS; r++){
+            for(int c = 0; c < COLS; c++){
+                fileArt.get(charArr[r][c]);
+            }
+        }
+        fileArt.close();
+        cout << "Done" << endl;
+    }    
+    else{
+        cout << "File does not exist." << endl;
+    }
 }
 
 void displayArt(char charArr[ROWS][COLS]){
@@ -45,6 +63,11 @@ void clearArray(char charArr[ROWS][COLS]){
 -sets every element within the character array as zero
 -Clears the character array so that a new file can be loaded in
 */
+    for (int r = 0; r < ROWS; r++){
+        for (int c = 0; c < COLS; c++){
+            charArr[r][c] = 0;
+        }
+    }
 }
 
 bool guessChange(char copyArr[ROWS][COLS]){
@@ -53,6 +76,8 @@ bool guessChange(char copyArr[ROWS][COLS]){
 -Asks user to guess the row the changed character is in
 -Then asks for the column
 -prints message that says if they guessed correctly or not.
+
+-Return T/F for whether the user guessed the correct coordinate
 */
 }
 
@@ -74,7 +99,6 @@ int main(){
     int command;
 
     while(isRunning){
-
         //main menu logic
         cout << "What would you like to do?" << endl;
         cout << "1. Load ASCII Art from File" << endl;
@@ -82,7 +106,6 @@ int main(){
         cout << "3. PLay Game" << endl;
         cout << "4. Exit" << endl;
         cin >> command; //command input
-
         //input validation for command input
         while (command < 1 || command > 4){
             cout << "What would you like to do?" << endl;
@@ -92,7 +115,6 @@ int main(){
             cout << "4. Exit" << endl;
             cin >> command;
         }
-
         //SWITCH STATEMENT FOR HANDLING COMMAND INPUT HERE
     }
 
